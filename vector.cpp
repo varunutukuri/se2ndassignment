59616cc40a6e43919a647d4929981725017801ae
varunutukuri <91932926+varunutukuri@users.noreply.github.com>
2025-02-05 10:10:18 +0530

    Update vector.cpp
    added comments

--- vector.cpp
+++ vector.cpp
@@ -1,3 +1,4 @@
+// Including necessary libraries for input-output operations
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,6 +6,7 @@
 using namespace std;
 
 template <typename T>
+// Function to print the elements of a vector
 void printVector(const vector<T>& v) {
     for (const auto& elem : v) {
         cout << elem << " ";
@@ -13,10 +15,14 @@ void printVector(const vector<T>& v) {
 }
 
 int main() {
+    // Initializing a vector with unsorted integers
     vector<int> numbers = {5, 3, 8, 1, 4, 7, 9, 2, 6};
+    
+    // Sorting the vector in ascending order
     sort(numbers.begin(), numbers.end());
     printVector(numbers);
     
+    // Key to search for in the sorted vector
     int key = 4;
     if (binary_search(numbers.begin(), numbers.end(), key)) {
         cout << key << " found in the vector." << endl;
